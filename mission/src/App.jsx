4cc2208260395b2c842e54bbbb637cea86b0dfbd
wooda0207 <wooda0207@gmail.com>
2024-04-03 15:34:35 +0900

    mission

---
+++ mission/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Welcome from "./components/welcome";
+
+function App() {
+  return (
+    <>
+      <Welcome name={"이정환"} isMember={true} />
+    </>
+  );
+}
+
+export default App;
