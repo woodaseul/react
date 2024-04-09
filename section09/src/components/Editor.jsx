ffa2fad7954d59ccf26fcc074c501fe8e50c8357
wooda0207 <wooda0207@gmail.com>
2024-04-09 20:15:51 +0900

    section9

---
+++ section09/src/components/Editor.jsx
@@ -0,0 +1,42 @@
+import { useRef, useState } from "react";
+import "./Editor.css";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // 엔터키 입력으로 새로운 투두가 추가
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onsubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        onKeyDown={onkeydown}
+        ref={contentRef}
+        value={content}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
